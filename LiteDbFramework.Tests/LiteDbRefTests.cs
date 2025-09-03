e37790e6d3f8839f5436b42666474c16dc797409
DustinLuck <115443+DustinLuck@users.noreply.github.com>
2025-09-03 16:44:46 -0700

    New ref tests

--- LiteDbFramework.Tests/LiteDbRefTests.cs
+++ LiteDbFramework.Tests/LiteDbRefTests.cs
@@ -1,3 +1,5 @@
+using System.Collections.Generic;
+
 namespace LiteDbFramework.Tests;
 
 public class LiteDbRefTests
@@ -15,19 +17,60 @@ public class LiteDbRefTests
         public Guid Id { get; set; } = Guid.NewGuid();
 
         [BsonRef("Parent")]
-        public Parent Reference { get; set; }
+        public Parent ParentReference { get; set; }
+    }
+
+    public class Building
+    {
+        [BsonId]
+        public Guid Id { get; set; } = Guid.NewGuid();
+        public string Name { get; set; }
+        [BsonRef]
+        public List<Room> Rooms { get; set; } = [];
+    }
+
+    public class Room
+    {
+        [BsonId]
+        public Guid Id { get; set; } = Guid.NewGuid();
+        public string Number { get; set; }
     }
 
     private class DbContext(string connectionString) : LiteDbContext(connectionString, ConfigureModel)
     {
         public LiteDbSet<Parent> Parents { get; [UsedImplicitly] private set; }
         public LiteDbSet<Child> Children { get; [UsedImplicitly] private set; }
+        public LiteDbSet<Building> Buildings { get; [UsedImplicitly] private set; }
+        public LiteDbSet<Room> Rooms { get; [UsedImplicitly] private set; }
 
         private static void ConfigureModel(ModelBuilder modelBuilder)
         {
             modelBuilder.Entity<Parent>(col => col.EnsureIndex(x => x.Id));
             modelBuilder.Entity<Child>(col => col.EnsureIndex(x => x.Id));
+            modelBuilder.Entity<Building>(col => col.EnsureIndex(x => x.Id));
+            modelBuilder.Entity<Room>(col => col.EnsureIndex(x => x.Id));
         }
+    }
+
+    [Fact]
+    public void InsertRecordWithReference_WithValidInput_ShouldReturnSuccess()
+    {
+        var path = Path.GetTempFileName();
+        var parent = new Parent { Name = "Parent 1" };
+        var child = new Child { ParentReference = parent };
+
+        using (var dbContext = new DbContext(path))
+        {
+            dbContext.Parents.Insert(parent);
+            dbContext.Children.Insert(child);
+        }
+
+        using var liteDb = new LiteDatabase(path);
+        var result = liteDb.GetCollection<Child>("Child").Include(x => x.ParentReference).FindById(child.Id);
+
+        Assert.NotNull(result);
+        Assert.NotNull(result.ParentReference);
+        Assert.Equal("Parent 1", result.ParentReference.Name);
     }
 
     [Fact]
@@ -35,7 +78,7 @@ public class LiteDbRefTests
     {
         var path = Path.GetTempFileName();
         var parent = new Parent { Name = "Parent 1" };
-        var child = new Child { Reference = parent };
+        var child = new Child { ParentReference = parent };
 
         using (var liteDb = new LiteDatabase(path))
         {
@@ -49,7 +92,35 @@ public class LiteDbRefTests
         var result = context.Children.WithReferences.FindById(child.Id);
 
         Assert.NotNull(result);
-        Assert.NotNull(result.Reference);
-        Assert.Equal("Parent 1", result.Reference.Name);
+        Assert.NotNull(result.ParentReference);
+        Assert.Equal("Parent 1", result.ParentReference.Name);
+    }
+
+    [Fact]
+    public void InsertRecordWithListReference_WithValidInput_ShouldReturnSuccess()
+    {
+        var path = Path.GetTempFileName();
+        var room1 = new Room { Number = "101" };
+        var room2 = new Room { Number = "102" };
+        var building = new Building
+        {
+            Name = "Building 1",
+            Rooms = [ room1, room2 ]
+        };
+
+        using (var dbContext = new DbContext(path))
+        {
+            dbContext.Rooms.Insert(room1);
+            dbContext.Rooms.Insert(room2);
+            dbContext.Buildings.Insert(building);
+        }
+
+        using var liteDb = new LiteDatabase(path);
+        var result = liteDb.GetCollection<Building>("Building").Include(x => x.Rooms).FindById(building.Id);
+        Assert.NotNull(result);
+        Assert.NotNull(result.Rooms);
+        Assert.Equal(2, result.Rooms.Count);
+        Assert.Equal("101", result.Rooms[0].Number);
+        Assert.Equal("102", result.Rooms[1].Number);
     }
 }
